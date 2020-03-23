3cc4c2fabaa506f382ef1ba229e3d1f2a93e955a
Artemis <hollyshort21@gmail.com>
2020-03-23 16:42:43 +0000

    added source code link and put invite link into about

--- bot.py
+++ bot.py
@@ -6,6 +6,13 @@ from config import TOKEN
 from cog import Stats
 
 
+INVITE = (
+    'https://discordapp.com/oauth2/authorize?client_id=691369979186249799'
+    '&permissions=604359745&scope=bot'
+)
+SOURCE = 'https://github.com/artemis21/coronastats'
+
+
 class Help(commands.DefaultHelpCommand):
     brief = 'Shows this message.'
     help = 'Provides help on a command.'
@@ -37,18 +44,6 @@ bot.add_cog(Stats())
 bot.help_command = Help()
 
 
-@bot.command(brief='Get the invite link.')
-async def invite(ctx):
-    '''Get the invite link to add the bot to another server.
-    '''
-    invite = (
-        'https://discordapp.com/oauth2/authorize?client_id=691369979186249799'
-        '&permissions=604359745&scope=bot'
-    )
-    e = discord.Embed(title='Invite', description=f'[Click here]({invite})')
-    await ctx.send(embed=e)
-
-
 @bot.command(brief='Basic bot info.')
 async def about(ctx):
     '''Information about the bot.
@@ -63,6 +58,8 @@ async def about(ctx):
     )
     e.add_field(name='Servers', value=len(bot.guilds))
     e.add_field(name='Users', value=len(bot.users))
+    e.add_field(name='Add Bot', value=f'**[Click here]({INVITE})**')
+    e.add_field(name='Source Code', value=f'**[Click here]({SOURCE})**')
     await ctx.send(embed=e)
 
 
